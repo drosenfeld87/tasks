3d04f07f86a83434851b985b37f97619f02da50d
debbie <drosenfeld87@gmail.com>
2018-02-11 12:41:57 -0500

    complete tasks exercise

--- todolist.rb
+++ todolist.rb
@@ -1,21 +1,28 @@
 require './task.rb'
 
-class Todolist
+class TodoList
 
-  def initialize(description, due_date)
-    @tasks = []
+  def initialize(description, tasks=[])
+    @tasks = tasks
   end
 
   def add_task(task)
     @tasks << task
   end
+
+  def tasks
+    @tasks
+  end
 end
 
-
-list = Todolist.new
+to_do_list = TodoList.new('somet description')
 
 task1 = Task.new('homework', 'Monday')
 task2 = Task.new('laundry', 'Saturday')
 task3 = Task.new('shopping', 'Sunday')
 
-puts task1.add_task
+puts task1.due_date
+
+to_do_list.add_task(task1)
+
+puts to_do_list.tasks
